97cde15a6d07932e8a0c67f18f87022245ed08ee
Alvaro Kannegießer Barco <akannegies@stud.hs-heilbronn.de>
2024-05-28 10:46:44 +0200

    buenos dias

--- Website/__init__.py
+++ Website/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from datetime import timedelta
 
 
 def create_app():
--- Website/views.py
+++ Website/views.py
@@ -1,7 +1,30 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, session
+import datetime
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
-def home():
+def base():
     return render_template("base.html")
+
+@views.route('/home')
+def home():
+    if 'start_time' not in session:
+        session['start_time'] = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    return render_template("home.html")
+
+@views.route('/video')
+def video():
+    return render_template("video.html")
+
+@views.route('/raetsel01')
+def raetsel01():
+    return render_template("Raetsel01.html")
+
+@views.route('/raetsel02')
+def raetsel02():
+    return render_template("Raetsel02.html")
+
+@views.route('/raetsel03')
+def raetsel03():
+    return render_template("Raetsel03.html")
