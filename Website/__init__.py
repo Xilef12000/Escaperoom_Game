d4098e373f528d270b4fd1088337d69bb6a51385
Alvaro Kannegießer Barco <akannegies@stud.hs-heilbronn.de>
2024-06-26 16:43:30 +0200

    SAVESTATE CHANGING CHECK_SOLUTIONS

--- Website/__init__.py
+++ Website/__init__.py
@@ -1,12 +1,11 @@
 from flask import Flask
-from datetime import timedelta
 
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'amorfati'
 
-    from .index import views
+    from Website.index import views
 
     app.register_blueprint(views, url_prefix='/')
 
