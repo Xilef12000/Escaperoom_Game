d295c9a9d070be6815d5ebed0c3e2da755829979
Alvaro Kannegießer Barco <akannegies@stud.hs-heilbronn.de>
2024-07-09 18:04:27 +0200

    SAVESTATE CHANGING CHECK_SOLUTIONS

--- Website/__init__.py
+++ Website/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 
 
 def create_app():
@@ -8,5 +9,8 @@ def create_app():
     from Website.index import views
 
     app.register_blueprint(views, url_prefix='/')
+
+    app.template_folder = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'templates')
+    app.static_folder = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'static')
 
     return app
