053beadb83f6470be8ce8d674b6441741ea40257
Xilef12000 <79515423+Xilef12000@users.noreply.github.com>
2024-07-10 17:49:52 +0200

    fix filename capitalization for linux

--- Website/index.py
+++ Website/index.py
@@ -21,15 +21,15 @@ def video():
 
 @views.route('/raetsel01')
 def raetsel01():
-    return render_template("Raetsel01.html")
+    return render_template("raetsel01.html")
 
 @views.route('/raetsel02')
 def raetsel02():
-    return render_template("Raetsel02.html")
+    return render_template("raetsel02.html")
 
 @views.route('/raetsel03')
 def raetsel03():
-    return render_template("Raetsel03.html")
+    return render_template("raetsel03.html")
 
 @views.route('/restart')
 def restart():
